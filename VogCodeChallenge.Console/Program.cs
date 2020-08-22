13b13ec986e5e4e35cfff7e81ff29e9f4e534b9f
bahar <bhr.taghavi@gmail.com>
2020-08-22 00:54:09 -0700

    replace code with foreach statement

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QuestionClass.TestQuestion();
+        }
+    }
+}
---
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            foreach (var name in NamesList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+
+}
