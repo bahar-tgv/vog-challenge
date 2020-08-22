90d70875d02120b46f64549a8f40fb0964274bf9
bahar <bhr.taghavi@gmail.com>
2020-08-21 17:40:35 -0700

    add models in a seperated class library

---
+++ VogCodeChallenge.Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VogCodeChallenge.Entities
+{
+    public class Department
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Address { get; set; }
+    }
+}
---
+++ VogCodeChallenge.Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace VogCodeChallenge.Entities
+{
+
+    public class Employee
+        {
+            [Required]
+            public int Id { get; set; }
+            [Required] [MaxLength(50)] public string FirstName { get; set; }
+
+            [Required] [MaxLength(50)] public string LastName { get; set; }
+
+            [Required] [MaxLength(50)] public string JobTitle { get; set; }
+
+            [MaxLength(100)] public string Address { get; set; }
+
+            [Required] public Department Department { get; set; }
+        }
+    }
+
